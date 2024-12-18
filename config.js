57d4adb68979cccf4163b1aa40995f0d15b54197
Sahil6q <ayan444sahil@gmail.com>
2024-12-18 20:29:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "add_your_session_id",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qp22bZ1/IMG-20241129-WA0428.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
-SUDO_NB: process.env.SUDO_NB || "94742398003",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+SESSION_ID: process.env.SESSION_ID || "HAd0xR7B#2UMuax7-27_1BRyGjNE_KnqJNZPDDZryDrUhEGzqlwk",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
+ALIVE_IMG: process.env.ALIVE_IMG || "SAHIL🙂🫠KING",
+ALIVE_MSG: process.env.ALIVE_IMG || "SAHIL🫠KING OF WHATSAPP ",
+SUDO_NB: process.env.SUDO_NB || "923495705077",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "fasi",
 MODE: process.env.MODE || "public",
-AUTO_VOICE:"true"
+AUTO_VOICE:"On"
 };
 
